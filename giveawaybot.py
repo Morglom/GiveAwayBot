993af5d4cae5068bb57059c78c7ff601acb990d9
Daniel Lloyd <Daniel-w-lloyd@hotmail.com>
2021-12-03 03:59:03 +1100

    Add description and dictionary

--- giveawaybot.py
+++ giveawaybot.py
@@ -3,6 +3,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import giveaway
 
+description = 'A bot for handling giveaways'
+
+# Dictionary for storing the ids of the management and announcement channels in the first two positions and the list of giveaways in the third position for each server
+guildDict = {}
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
