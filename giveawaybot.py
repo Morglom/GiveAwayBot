a0f95e45d99da76aca40d037d40a052b78e08e40
Daniel Lloyd <Daniel-w-lloyd@hotmail.com>
2021-12-03 04:00:43 +1100

    Add function for bot joining server
    Will create needed role and channels if they don't already exist

--- giveawaybot.py
+++ giveawaybot.py
@@ -19,4 +19,27 @@ async def on_ready():
     print(f'{bot.user} is connected')
 
 # When bot joins a server it creates a management role, a management channel and an announcement channel for giveaways
+@bot.event
+async def on_guild_join(self, guild)
+        # Check if the role already exists and if not create it
+        manager_role = get(ctx.guild.roles, name="Giveaway Manager")
+        if manager_role is None
+            manager_role = await guild.create_role(name="Giveaway Manager")
+        
+        # Overwrites to limit access to the management channel
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            guild.me: discord.PermissionOverwrite(read_messages=True),
+            manager_role: discord.PermissionOverwrite(read_messages=True)
+        }
+        
+        # Check if channels exist and if not create them
+        if get(guild.text_channels, name='giveaway-management') is None:
+            await guild.create_text_channel("giveaway-management", overwrites = overwrites)
+        if get(guild.text_channels, name='giveaways') is None:
+            await guild.create_text_channel("giveaways")
+        
+        # Add details of the guild to the dictionary
+        channelDict[guild.id] = [ get(guild.text_channels, name='giveaway-management').id , get(guild.text_channels, name='giveaways').id , []]
+    
 bot.run(TOKEN)
