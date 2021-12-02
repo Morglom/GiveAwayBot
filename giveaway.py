45280d2ee094e2ee549f8f429a69a6893dfeeb68
Daniel Lloyd <Daniel-w-lloyd@hotmail.com>
2021-12-02 21:41:41 +1100

    Object class for giveaway

---
+++ giveaway.py
@@ -0,0 +1,20 @@
+class giveaway:
+    id
+    name
+    description
+    channel
+    time
+    date
+    participants
+    public
+    
+    def __init__(self, name, description, channel, time, date, participants, public)
+        self.id = id
+        self.name = name
+        self.description
+        self.channel = channel
+        self.time = time
+        self.date = date
+        self.participants = participants
+        self.public = public
+    
