6ced1d3cbeb48e512ded9c6295eb448852dbfe04
Daniel Lloyd <Daniel-w-lloyd@hotmail.com>
2021-12-03 23:09:10 +1100

    Add schedules

--- giveaway.py
+++ giveaway.py
@@ -8,12 +8,19 @@ class giveaway:
     public
     # Whether a participant can win multiple rounds
     repeats
+    # Whether an announcement should be scheduled at these timestamp
+    week
+    day
+    hour
     
-    def __init__(self, name, description, time, date, participants, public, repeats)
+    def __init__(self, name, description, time, date, participants, public, repeats, week, day, hour)
         self.name = name
         self.description
         self.time = time
         self.participants = participants
         self.public = public
         self.repeats = repeats
+        self.week
+        self.day
+        self.hour
     
